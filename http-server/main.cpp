14c487eacfee9d25941be5c43cb927cfc038c175
shaw <vints327@163.com>
2025-01-13 10:33:23 +0800

    更新

--- http-server/main.cpp
+++ http-server/main.cpp
@@ -1,7 +1,8 @@
 // main.cpp
 #include <iostream>
-// #include <httplib.h>
-#include "httplib.h" // 修改为直接包含 httplib.h
+#include <httplib.h>
+// #include "D:/vcpkg/installed/x64-windows/include/httplib.h"
+// #include "httplib.h" // 修改为直接包含 httplib.h
 // #include "cpp-httplib/cpp-httplib.h"
 
 int main()
@@ -22,7 +23,10 @@ int main()
             res.set_content("Hello, " + name + "!", "text/plain");
         } });
 
-    svr.listen("0.0.0.0", 8080);
+    // svr.listen("0.0.0.0", 8080);
+    svr.listen("localhost", 8080);
 
     return 0;
 }
+
+// g++ E:\repository\_code-source\_C_C++\_learn\http-server\main.cpp -o main -I"D:/vcpkg/installed/x64-windows/include" -lws2_32

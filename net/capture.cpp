8ad36db05f6bb2cfa8ee0ed6afae6daf31d002cf
shaw <vints327@163.com>
2025-01-22 17:42:42 +0800

    新增tcp、udp协议解析

--- net/capture.cpp
+++ net/capture.cpp
@@ -26,13 +26,45 @@ void printAndLog(const std::string& message) {
     }
 }
 
-void parseEthernetHeader(const u_char* data) {
-    struct EthernetHeader {
-        u_char dest[6];
-        u_char src[6];
-        u_short type;
-    };
+struct EthernetHeader {
+    u_char dest[6];
+    u_char src[6];
+    u_short type;
+};
 
+struct IPHeader {
+    u_char versionAndHeaderLength;
+    u_char tos;
+    u_short totalLength;
+    u_short id;
+    u_short flagsAndOffset;
+    u_char ttl;
+    u_char protocol;
+    u_short checksum;
+    u_int srcAddr;
+    u_int destAddr;
+};
+
+struct TCPHeader {
+    u_short srcPort;
+    u_short destPort;
+    u_int seqNum;
+    u_int ackNum;
+    u_char offsetAndReserved;
+    u_char flags;
+    u_short window;
+    u_short checksum;
+    u_short urgentPtr;
+};
+
+struct UDPHeader {
+    u_short srcPort;
+    u_short destPort;
+    u_short length;
+    u_short checksum;
+};
+
+void parseEthernetHeader(const u_char* data) {
     const EthernetHeader* eth = reinterpret_cast<const EthernetHeader*>(data);
 
     std::ostringstream ss;
@@ -52,19 +84,6 @@ void parseEthernetHeader(const u_char* data) {
 }
 
 void parseIPHeader(const u_char* data) {
-    struct IPHeader {
-        u_char versionAndHeaderLength;
-        u_char tos;
-        u_short totalLength;
-        u_short id;
-        u_short flagsAndOffset;
-        u_char ttl;
-        u_char protocol;
-        u_short checksum;
-        u_int srcAddr;
-        u_int destAddr;
-    };
-
     const IPHeader* ip = reinterpret_cast<const IPHeader*>(data);
 
     int headerLength = (ip->versionAndHeaderLength & 0x0F) * 4;
@@ -86,6 +105,51 @@ void packetHandler(u_char* param, const struct pcap_pkthdr* header, const u_char
 
     if (header->caplen > 14) {
         parseIPHeader(pktData + 14); // Ethernet header is 14 bytes
+
+        const IPHeader* ip = reinterpret_cast<const IPHeader*>(pktData + 14);
+        int ipHeaderLength = (ip->versionAndHeaderLength & 0x0F) * 4;
+        int transportHeaderOffset = 14 + ipHeaderLength;
+
+        if (header->caplen > transportHeaderOffset) {
+            switch (ip->protocol) {
+                case IPPROTO_TCP: {
+                    const TCPHeader* tcp = reinterpret_cast<const TCPHeader*>(pktData + transportHeaderOffset);
+                    std::ostringstream ss;
+                    ss << "TCP Header:";
+                    ss << "\n  Source Port: " << ntohs(tcp->srcPort);
+                    ss << "\n  Destination Port: " << ntohs(tcp->destPort);
+                    ss << "\n  Sequence Number: " << ntohl(tcp->seqNum);
+                    ss << "\n  Acknowledgment Number: " << ntohl(tcp->ackNum);
+                    ss << "\n  Data Offset: " << ((tcp->offsetAndReserved >> 4) * 4) << " bytes";
+                    ss << "\n  Flags: ";
+                    if (tcp->flags & 0x01) ss << "FIN ";
+                    if (tcp->flags & 0x02) ss << "SYN ";
+                    if (tcp->flags & 0x04) ss << "RST ";
+                    if (tcp->flags & 0x08) ss << "PSH ";
+                    if (tcp->flags & 0x10) ss << "ACK ";
+                    if (tcp->flags & 0x20) ss << "URG ";
+                    ss << "\n  Window Size: " << ntohs(tcp->window);
+                    ss << "\n  Checksum: " << ntohs(tcp->checksum);
+                    ss << "\n  Urgent Pointer: " << ntohs(tcp->urgentPtr);
+                    printAndLog(ss.str());
+                    break;
+                }
+                case IPPROTO_UDP: {
+                    const UDPHeader* udp = reinterpret_cast<const UDPHeader*>(pktData + transportHeaderOffset);
+                    std::ostringstream ss;
+                    ss << "UDP Header:";
+                    ss << "\n  Source Port: " << ntohs(udp->srcPort);
+                    ss << "\n  Destination Port: " << ntohs(udp->destPort);
+                    ss << "\n  Length: " << ntohs(udp->length);
+                    ss << "\n  Checksum: " << ntohs(udp->checksum);
+                    printAndLog(ss.str());
+                    break;
+                }
+                default:
+                    printAndLog("Unsupported protocol: " + std::to_string(ip->protocol));
+                    break;
+            }
+        }
     }
 }
 
@@ -117,7 +181,8 @@ int main() {
     pcap_if_t* device;
     int i = 0;
     for (device = allDevices; device != nullptr; device = device->next) {
-        printAndLog(std::to_string(i++) + ": " + device->name);
+        std::string desc = device->description ? device->description : "No description available";
+        printAndLog(std::to_string(i++) + ": " + device->name + " (" + desc + ")");
     }
 
     printAndLog("\nSelect a device by index: ");
